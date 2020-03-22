c7e81b83ab6e820545567e52e45035e140467f47
Bob (IzotovAA) <bob@example.com>
2020-03-22 20:54:17 +0300

    Code: добавил минимум

--- main.cpp
+++ main.cpp
@@ -17,14 +17,21 @@ int main()
          znacheniya[1]=a - b;
          znacheniya[2]=a * b;
          znacheniya[3]=a / b;
+
          double max=znacheniya[0];
+         double min=znacheniya[0];
          for(size_t i=0;i<3;i++)
          {
+             if (min>znacheniya[i+1])
+             {
+                 min=znacheniya[i+1];
+             }
              if (max<znacheniya[i+1])
              {
                  max=znacheniya[i+1];
              }
          }
+         cout <<"Min="<<min<<'\n';
          cout <<"Max="<<max<<'\n';
     return 0;
 }
